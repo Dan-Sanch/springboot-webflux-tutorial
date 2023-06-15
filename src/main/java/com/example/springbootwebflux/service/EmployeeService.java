2dfd15a7d4fbb4e226a8c9d251458dbf746b5c16
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-15 15:36:08 -0600

    Implemented Employee service layer

---
+++ src/main/java/com/example/springbootwebflux/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.springbootwebflux.service;
+
+import com.example.springbootwebflux.dto.EmployeeDto;
+import reactor.core.publisher.Mono;
+
+public interface EmployeeService {
+    Mono<EmployeeDto> saveEmployee(EmployeeDto employeeDto);
+}
---
+++ src/main/java/com/example/springbootwebflux/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.springbootwebflux.service.impl;
+
+import com.example.springbootwebflux.dto.EmployeeDto;
+import com.example.springbootwebflux.entity.Employee;
+import com.example.springbootwebflux.mapper.EmployeeMapper;
+import com.example.springbootwebflux.repository.EmployeeRepository;
+import com.example.springbootwebflux.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public Mono<EmployeeDto> saveEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+
+        Mono<Employee> savedEmployee = employeeRepository.save(employee);
+        Mono<EmployeeDto> savedEmployeeDto = savedEmployee.map(EmployeeMapper::mapToEmployeeDto);
+
+        return savedEmployeeDto;
+    }
+}
