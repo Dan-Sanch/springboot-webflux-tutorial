b0d1d82be82afd86f4c0c8122514993e40ddb378
Dan-Sanch <59940500+Dan-Sanch@users.noreply.github.com>
2023-06-15 16:11:59 -0600

    Implemented save Employee reactive endpoint, controller

---
+++ src/main/java/com/example/springbootwebflux/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.example.springbootwebflux.controller;
+
+import com.example.springbootwebflux.dto.EmployeeDto;
+import com.example.springbootwebflux.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
+        return employeeService.saveEmployee(employeeDto);
+    }
+}
